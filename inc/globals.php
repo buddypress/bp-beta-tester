4eec73c4af3760b69785a8630a9071663b86279b
imath <math.viet@gmail.com>
2024-07-08 20:23:52 +0200

    Bump plugin version to 1.3.0
    * Bump plugin version to 1.3.0
    
    Update some composer dependencies
    Refresh pot file
    Update node/npm required engines
    
    * Fix a wrong upgrade notice naming
    
    Props @emaralive @vapvarun

--- inc/globals.php
+++ inc/globals.php
@@ -17,7 +17,7 @@ defined( 'ABSPATH' ) || exit;
 function bp_beta_tester_globals() {
 	$bpbt = bp_beta_tester();
 
-	$bpbt->version = '1.3.0-alpha';
+	$bpbt->version = '1.3.0';
 
 	// Paths.
 	$bpbt->dir      = plugin_dir_path( dirname( __FILE__ ) );
